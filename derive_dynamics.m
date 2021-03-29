2029de606cb7717846174a13b455a110db16b077
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-03-29 14:58:53 +0200

    Save B and G matrices to matlab function in derive_dynamics.m

--- derive_dynamics.m
+++ derive_dynamics.m
@@ -33,7 +33,9 @@ x_m2 = x_m1 + [(sin(theta2)-sin(theta1))/kappa2;
 J_m0_P = jacobian(x_m0, q);
 J_m1_P = jacobian(x_m1, q);
 J_m2_P = jacobian(x_m2, q);
-B = J_m0_P'*m(1)*J_m0_P + J_m1_P'*m(2)*J_m1_P + J_m2_P'*m(3)*J_m2_P;
+
+% B(q) matrix in EOM
+B = simplify(J_m0_P'*m(1)*J_m0_P + J_m1_P'*m(2)*J_m1_P + J_m2_P'*m(3)*J_m2_P);
 
 % Kinetic energy of the system
 T = 1/2*qdot'*B*qdot;
@@ -41,12 +43,30 @@ T = 1/2*qdot'*B*qdot;
 % Potential energy of the system
 U_g = m0*g'*x_m0 + m1*g'*x_m1 + m2*g'*x_m2;
 U_k = 1/2*(k0*theta0^2+k1*theta1^2+k2*theta2^2);
-U = U_g + U_k;
+U = simplify(U_g + U_k);
 
 % Langrangian
 L = T - U;
 
 % EOM: B(q)*qddot + C(q,qdot)*qdot + G(q) = tau
 
+% C(q, qdot) matrix in EoM
+C = [0; 0; 0];
+% Jdot_m0_P = jacobian(J_m0_P, q) * qdot
+% C = J_m0_P'*m(1)*Jdot_m0_P + J_m1_P'*m(2)*Jdot_m1_P + J_m2_P'*m(3)*Jdot_m2_P
+
 % G(q) vector in EoM
 G = jacobian(U, q)';
+
+%% Generate matlab functions
+fname = mfilename;
+fpath = mfilename('fullpath');
+dpath = strrep(fpath, fname, '');
+
+fprintf('Generating eom scripts... ');
+fprintf('B... ');
+matlabFunction(B, 'vars', {q, alpha, l, m}, 'file', strcat(dpath,'/B_fun'), 'Optimize', false);
+% fprintf('C... ');
+% matlabFunction(C, 'vars', {q, qdot, alpha, l, m}, 'file', strcat(dpath,'/C_fun'), 'Optimize', false);
+fprintf('G... ');
+matlabFunction(G, 'vars', {q, alpha, l, m, k, g}, 'file', strcat(dpath,'/G_fun'), 'Optimize', false);
