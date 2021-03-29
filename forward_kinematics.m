2896a8067313b2ba440f0756f3f1afd7101efd60
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-03-29 10:39:55 +0200

    Update forward kinematics to use relative angles as generalized coordinates

--- forward_kinematics.m
+++ forward_kinematics.m
@@ -1,14 +1,14 @@
-function x = forward_kinematics(theta_0, k, l)
+function x = forward_kinematics(alpha, l, kappa)
     % Theta_0: 1x1 of initial offset angle [rad]
     % k: nx3 curvatures of each of 3 segments
     % l: nx3 lengths of each of 3 segments
     
-    theta0 = theta_0 + k(:, 1).*l(:, 1);
-    theta1 = theta0 + k(:, 2).*l(:, 2);
-    theta2 = theta1 + k(:, 3).*l(:, 3);
-    x0 = [(sin(theta0)-sin(theta_0))./k(:,1), -(cos(theta0)-cos(theta_0))./k(:,1)];
-    x1 = x0 + [(sin(theta1)-sin(theta0))./k(:,2), -(cos(theta1)-cos(theta0))./k(:,2)];
-    x2 = x1 + [(sin(theta2)-sin(theta1))./k(:,3), -(cos(theta2)-cos(theta1))./k(:,3)];
+    theta0 = alpha + kappa(:, 1).*l(:, 1);
+    theta1 = theta0 + kappa(:, 2).*l(:, 2);
+    theta2 = theta1 + kappa(:, 3).*l(:, 3);
+    x0 = [(sin(theta0)-sin(alpha))./kappa(:,1), -(cos(theta0)-cos(alpha))./kappa(:,1)];
+    x1 = x0 + [(sin(theta1)-sin(theta0))./kappa(:,2), -(cos(theta1)-cos(theta0))./kappa(:,2)];
+    x2 = x1 + [(sin(theta2)-sin(theta1))./kappa(:,3), -(cos(theta2)-cos(theta1))./kappa(:,3)];
     x = x2;
 end
 
