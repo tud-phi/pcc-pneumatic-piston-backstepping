55269021fe464e18620884efc89a28e0ed741059
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-05-26 15:29:43 +0200

    Implement realistic parameters for soft robot

--- config.m
+++ config.m
@@ -4,5 +4,5 @@ n_C = 6; % number of chambers
 % q_max = pi/2; % maximum assumed generalized coordinates
 
 % parameters
-l = [1;1;1]; % length of every segment
-rho = [1;1;1]; % mass density (per length) of every segment
+l = 0.110*[1;1;1]; % length of every segment
+rho = 0.99*[1;1;1]; % mass density (per length) of every segment
