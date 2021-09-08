6ea7c2f51ff2a52811125abd765a3b55038f7104
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-08 11:40:50 +0200

    Initialise pistons at position corresponding to q_eq (equilibrium configuration)

--- config.m
+++ config.m
@@ -9,6 +9,7 @@ rho = 0.99*[1;1;1]; % mass density (per length) of every segment
 
 % equilibrium configuration (experimentally determined)
 q_eq = [-108; 21.5; -3.4]/180*pi;
+mu_p_eq = [0.12135; 0.12866; 0.12574; 0.12426; 0.12489; 0.12511];
 
 % minimal q for stability of B, C and G matrices
 tol_q_stable = 1/180*pi;
