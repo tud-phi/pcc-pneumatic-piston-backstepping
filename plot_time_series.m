a0ed723bf474247e90c5242258cde368d1864284
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-08 17:01:04 +0200

    Remove title and split into individual figures for time series plots

--- plot_time_series.m
+++ plot_time_series.m
@@ -5,14 +5,11 @@ startup
 out_backstepping = load('data/out_backstepping.mat').out;
 out_pid = load('data/out_backstepping_k1-250_k2-100.mat').out;
 
-f = figure;
-f.Position(3:4) = [560 2*420];
+f = figure('Name', 'Configuration time series');
+% f.Position(3:4) = [560 2*420];
 set(gcf,'color','w');
-num_plots = 3;
-
-q_subplot = subplot(num_plots, 1, 1);
 hold on
-title('Configuration $q$', Interpreter='latex');
+% title('Configuration $q$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$q$ [$^{\circ}$]', Interpreter='latex');
 ylim([-120 +45]);
@@ -26,9 +23,10 @@ lgd.FontSize = 8;
 hold off
 
 
-mu_p_subplot = subplot(num_plots, 1, 2);
+f = figure('Name', 'Piston position time series');
+set(gcf,'color','w');
 hold on
-title('Piston position $\mu_\mathrm{p}$', Interpreter='latex');
+% title('Piston position $\mu_\mathrm{p}$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$\mu_\mathrm{p}$ [mm]', Interpreter='latex');
 plot(out_backstepping.mu_p*1000, '--');
@@ -44,12 +42,13 @@ lgd = legend('$\mu_{\mathrm{p},1,\mathrm{L}}$', ...
 lgd.FontSize = 8;
 hold off
 
-f_p_subplot = subplot(num_plots, 1, 3);
+f = figure('Name', 'Actuation force time series');
+set(gcf,'color','w');
 hold on
-title('Actuation force $f_\mathrm{p}$', Interpreter='latex');
+% title('Actuation force $f_\mathrm{p}$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$f_\mathrm{p}$ [N]', Interpreter='latex');
-ylim([-500 0]);
+ylim([-350 0]);
 plot(out_backstepping.f_p, '--');
 set(gca, 'ColorOrderIndex', 1)
 plot(out_pid.f_p, ':');
