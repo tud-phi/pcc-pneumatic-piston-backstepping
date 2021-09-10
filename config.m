3c91d31a88a7a4a3d591422e81ce28babcbb0047
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-10 14:29:07 +0200

    Increase tol_q_stable to 3 degree

--- config.m
+++ config.m
@@ -12,4 +12,4 @@ q_eq = [-108; 21.5; -3.4]/180*pi;
 mu_p_eq = [0.12135; 0.12866; 0.12574; 0.12426; 0.12489; 0.12511];
 
 % minimal q for stability of B, C and G matrices
-tol_q_stable = 1/180*pi;
+tol_q_stable = 3/180*pi;
