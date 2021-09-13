eae4a01895816c282570a18f823deda0756cf984
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-13 15:43:49 +0200

    Compute a as part of identification of actuator to configuration mapping

--- identify_actuator_to_configuration_mapping.m
+++ identify_actuator_to_configuration_mapping.m
@@ -12,8 +12,14 @@ max_s_step_gain = 0.005;
 num_steps = 50;
 s_array = zeros(num_steps, n_b);
 q_array = zeros(num_steps, n_b);
+a_array = zeros(num_steps, n_b);
 idx = 0;
 for s_step_magnitude = -max_s_step_gain:2*max_s_step_gain/num_steps:max_s_step_gain
+    % skip configurations close to zero because of numerical issues
+    if abs(s_step_magnitude/max_s_step_gain) < 0.05
+       continue 
+    end
+    
     idx = idx + 1;
     
     s_step_magnitude
@@ -21,15 +27,13 @@ for s_step_magnitude = -max_s_step_gain:2*max_s_step_gain/num_steps:max_s_step_g
     s_step_gain = s;
     s_array(idx, :) = s;
     
-    % skip configurations close to zero because of numerical issues
-    if abs(s_step_magnitude/max_s_step_gain) < 0.05
-       q_array(idx, :) = [0, 0, 0];
-       continue 
-    end
-    
     out = sim('pcc_actuator_to_configuration_mapping_identification');
     q_steady_state = out.q.Data(end, :);
     q_array(idx, :) = q_steady_state';
+    
+    % a = k / s * q
+    a_idx = out.k.Data(end, :) ./ s' .* q_steady_state;
+    a_array(idx, :) = a_idx;
 end
 
 figure;
@@ -39,10 +43,12 @@ set(gcf,'color','w');
 
 hold on
 for i=1:n_b
-    plot(s_array(:, i)*1000, q_array(:, i), '-o')
+    plot(s_array(1:idx, i)*1000, q_array(1:idx, i), '-o')
 end
 
 xlabel('piston step [mm]');
 ylabel('configuration [rad]');
 legend('q1', 'q2', 'q3');
 hold off
+
+a = mean(a_array(1:idx, :), 1)
