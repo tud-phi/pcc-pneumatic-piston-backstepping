a09064b53850ef5f911712c6eb106e87106d3ab3
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-11-05 11:28:00 +0100

    Update y limits of time series plots v2

--- plot_time_series_v2.m
+++ plot_time_series_v2.m
@@ -1,27 +1,30 @@
 %% Run startup
 startup
 
+figureWidth = 560;
+figureHeight = 240;
+
 %% Plot time
 % standard actuator dynamics configuration (m_p = 0.19kg)
 out = load('data/out_backstepping_v3_d_p-10000.mat').out;
-% out = load('data/out_pid.mat').out;
+% out = load('data/out_pid_coupling_aware_d_p-10000.mat').out;
 % out = load('data/out_pid_full_system_d_p-10000.mat').out;
 % increase piston mass m_p to 0.5kg
 % out = load('data/out_backstepping_v3_m_p-0.5_d_p-10000.mat').out;
-% out = load('data/out_pid_m_p-0.5_d_p-10000_untuned.mat').out;
+% out = load('data/out_pid_coupling_aware_m_p-0-5_d_p-10000.mat').out;
 % out = load('data/out_pid_full_system_m_p-0.5_d_p-10000_untuned.mat').out;
 
 f = figure('Name', 'Configuration time series');
 grid on
 box on
-f.Position(3:4) = [560 315];
+f.Position(3:4) = [figureWidth figureHeight];
 set(gcf,'color','w');
 hold on
 % title('Configuration $q$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$q$ [rad]', Interpreter='latex');
 % ylim([-2.0071 +0.7854]);
-ylim([-2.0071 +1.8]);
+ylim([-2.1 +2.5]);
 plot(out.q_ref, LineWidth=1.75);
 set(gca, 'ColorOrderIndex', 1)
 plot(out.q, ':', LineWidth=1.75);
@@ -34,7 +37,7 @@ hold off
 
 
 f = figure('Name', 'Piston position time series');
-f.Position(3:4) = [560 315];
+f.Position(3:4) = [figureWidth figureHeight];
 grid on
 box on
 set(gcf,'color','w');
@@ -42,6 +45,7 @@ hold on
 % title('Piston position $\mu_\mathrm{p}$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$\mu_\mathrm{p}$ [mm]', Interpreter='latex');
+ylim([80 200]);
 plot(out.mu_p*1000, ':', LineWidth=1.75);
 lgd = legend('$\mu_{\mathrm{p},1,\mathrm{L}}$', ...
              '$\mu_{\mathrm{p},1,\mathrm{R}}$', ...
@@ -54,7 +58,7 @@ lgd.FontSize = 11;
 hold off
 
 f = figure('Name', 'Actuation force time series');
-f.Position(3:4) = [560 315];
+f.Position(3:4) = [figureWidth figureHeight];
 grid on
 box on
 set(gcf,'color','w');
@@ -62,7 +66,7 @@ hold on
 % title('Actuation force $f_\mathrm{p}$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$f_\mathrm{p}$ [N]', Interpreter='latex');
-ylim([-350 0]);
+ylim([-400 50]);
 plot(out.f_p, ':', LineWidth=1.75);
 lgd = legend('$f_{\mathrm{p},1,\mathrm{L}}$', ...
              '$f_{\mathrm{p},1,\mathrm{R}}$', ...
@@ -75,7 +79,7 @@ lgd.FontSize = 11;
 hold off
 
 f = figure('Name', 'Pressure time series');
-f.Position(3:4) = [560 315];
+f.Position(3:4) = [figureWidth figureHeight];
 grid on
 box on
 set(gcf,'color','w');
@@ -83,7 +87,7 @@ hold on
 % title('Actuation force $f_\mathrm{p}$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$p$ [bar]', Interpreter='latex');
-% ylim([1 5]);
+ylim([2.25 5.5]);
 plot(out.p ./ 10^5, ':', LineWidth=1.75);
 lgd = legend('$p_{1,\mathrm{L}}$', ...
              '$p_{1,\mathrm{R}}$', ...
@@ -91,6 +95,6 @@ lgd = legend('$p_{1,\mathrm{L}}$', ...
              '$p_{2,\mathrm{R}}$', ...
              '$p_{3,\mathrm{L}}$', ...
              '$p_{3,\mathrm{R}}$', ...
-             Interpreter='latex', Location='southwest');
+             Interpreter='latex', Location='northwest');
 lgd.FontSize = 11;
 hold off
