b3ba4d71bc2f6ea869de0bb8426bda3b832d20a7
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-11-09 11:13:14 +0100

    Decrease ylimit of configuration plots

--- plot_time_series_v2.m
+++ plot_time_series_v2.m
@@ -6,13 +6,13 @@ figureHeight = 222; % same aspect ratio, but smaller: 185
 
 %% Plot time
 % standard actuator dynamics configuration (m_p = 0.19kg)
-% out_sim = load('data/out_backstepping_v3_d_p-10000.mat').out;
+out_sim = load('data/out_backstepping_v3_d_p-10000.mat').out;
 % out_sim = load('data/out_pid_coupling_aware_d_p-10000.mat').out;
 % out_sim = load('data/out_pid_full_system_d_p-10000.mat').out;
 % increase piston mass m_p to 0.5kg
 % out_sim = load('data/out_backstepping_v3_m_p-0.5_d_p-10000.mat').out;
 % out_sim = load('data/out_pid_coupling_aware_m_p-0-5_d_p-10000.mat').out;
-out_sim = load('data/out_pid_full_system_m_p-0.5_d_p-10000_untuned.mat').out;
+% out_sim = load('data/out_pid_full_system_m_p-0.5_d_p-10000_untuned.mat').out;
 
 f = figure('Name', 'Configuration time series');
 grid on
@@ -23,14 +23,13 @@ hold on
 % title('Configuration $q$', Interpreter='latex');
 xlabel('Time [s]', Interpreter='latex');
 ylabel('$q$ [rad]', Interpreter='latex');
-% ylim([-2.0071 +0.7854]);
-ylim([-2.1 +2.5]);
+ylim([-2.1 +1.05]);
 plot(out_sim.q_ref, LineWidth=1.75);
 set(gca, 'ColorOrderIndex', 1)
 plot(out_sim.q, ':', LineWidth=1.75);
 lgd = legend('$\bar{q}_1$','$\bar{q}_2$', '$\bar{q}_3$', ...
              '$q_{1}$', '$q_{2}$', '$q_{3}$', ...
-             Interpreter='latex', Location='northwest', ...
+             Interpreter='latex', Location='south', ...
              Orientation='horizontal', NumColumns=3);
 lgd.FontSize = 11;
 hold off
